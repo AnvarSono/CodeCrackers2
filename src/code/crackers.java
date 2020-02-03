0788d877b37934c2b7f9d70b6e84a38cdf5c99ff
Norbert <https://github.com/norbertBirsan/VeryFirst.git>
2020-02-03 14:28:13 -0600

    Alio

--- src/code/crackers.java
+++ src/code/crackers.java
@@ -5,6 +5,7 @@ public class crackers {
         System.out.println("What's up world!");
         System.out.println("Cybertek");
         System.out.println("Iryna's code");
+        System.out.println("Norbert");
 
     }
 }
